017a21c23e4f91a394c5131f1cbc0e61197f57b2
Umrzoq Toshkentov <utoshkentov@gmail.com>
2021-10-14 14:06:56 +0500

    fix: awarding section posting to server

--- src/pages/CompanyPages/settings/screens/AwardingSection/hooks/useAwards.ts
+++ src/pages/CompanyPages/settings/screens/AwardingSection/hooks/useAwards.ts
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { TOTAL_FIELDS_PATTERN, IForm } from "../constants";
 import { useForm, useWatch } from "react-hook-form";
 import { useQuery, useMutation } from "react-query";
@@ -14,19 +13,17 @@ const useAwards = () => {
     refetchOnWindowFocus: false,
     onSuccess: (data) => {
       data.data.data.rewards.forEach((element: any) => {
-        if (element.isActive) {
-          if (element.rewardType === 1) {
-            setValue("inviteCheck", true);
-          }
-          if (element.rewardType === 2) {
-            setValue("recommendCheck", true);
-          }
-          if (element.rewardType === 3) {
-            setValue("birthdayCheck", true);
-          }
-          if (element.rewardType === 4) {
-            setValue("vipCheck", true);
-          }
+        if (element.rewardType === 1) {
+          setValue("inviteCheck", element.isActive);
+        }
+        if (element.rewardType === 2) {
+          setValue("recommendCheck", element.isActive);
+        }
+        if (element.rewardType === 3) {
+          setValue("birthdayCheck", element.isActive);
+        }
+        if (element.rewardType === 4) {
+          setValue("vipCheck", element.isActive);
         }
       });
       let result = data?.data?.data?.rewards;
@@ -39,16 +36,17 @@ const useAwards = () => {
       );
       let forBeforeDay = result.find((item: any) => item?.levels[0]?.beforeDay);
 
-      console.log(result, "forSecond");
+      console.log(forSecond, "forSecond");
       //let forDescription = result.find((item:any)=>item.)  ;
       // let forMoreThan :any = result.find((item: any) => item?.levels[0]?.requirements[0]?.amount);
-      // setValue("ifMoreThan", forMoreThan?.levels[0]?.requirements[0]?.amount);
+      setValue("ifMoreThan", forFourth?.levels[0]?.requirements[0]?.amount);
       setValue("awardSizeFirst", forFirst?.amount);
-      setValue("awardSizeSecond", forThird?.amount);
-      setValue("awardSizeThird", forThird?.amount);
+      setValue("awardSizeSecond", forCongrat?.amount);
+      setValue("awardSizeThird", forSecond?.amount);
       setValue("awardSizeFourth", forFourth?.amount);
       setValue("description", forCongrat?.levels[0]?.congratulationText);
       setValue("payfor", forBeforeDay?.levels[0]?.beforeDay);
+      setValue("awardLimit", forSecond?.levels[0]?.limitCountReward);
     },
   });
 
@@ -95,7 +93,8 @@ const useAwards = () => {
     }
     if (data.awardSizeSecond) {
       TOTAL_FIELDS_PATTERN[1].amount = +data.awardSizeSecond;
-
+      TOTAL_FIELDS_PATTERN[1].levels[0].limitCountReward = data.awardLimit;
+      //   awardLimit
       TOTAL_FIELDS_PATTERN[1].isActive = data.recommendCheck;
     }
     if (data.awardSizeThird) {
