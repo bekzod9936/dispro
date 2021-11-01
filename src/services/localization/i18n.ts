facf8aaf7757d9e3ffcb22bd12d7f79b13a2e010
Umrzoq Toshkentov <utoshkentov@gmail.com>
2021-11-01 12:45:39 +0500

    fix: loyality program

--- src/services/localization/i18n.ts
+++ src/services/localization/i18n.ts
@@ -1,8 +1,5 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import XHR from "i18next-xhr-backend";
 import localeRU from "./locales/ru.json";
 import localeUZ from "./locales/uz.json";
 
