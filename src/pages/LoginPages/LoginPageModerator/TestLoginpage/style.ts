8415b307da4a46a229b5f2974a0c7fcc13fc216c
bekzod9936 <bekzodnorkulov9936@gmail.com>
2021-11-10 10:07:53 +0500

    add: mobile

--- src/pages/LoginPages/LoginPageModerator/TestLoginpage/style.ts
+++ src/pages/LoginPages/LoginPageModerator/TestLoginpage/style.ts
@@ -54,12 +54,14 @@ export const RightSide = styled.div`
   border-top-left-radius: 10%;
   display: flex;
   flex-direction: column;
+
   @media (max-width: ${device.mobile}) {
     background: white;
     border-top-left-radius: 0;
     align-items: center;
     justify-content: center;
     flex: 1;
+    padding: env(safe-area-inset-top) 0 0 0;
   }
 `;
 
@@ -127,6 +129,7 @@ export const WrapSelect = styled.div`
   align-items: center;
   flex-shrink: 1;
   padding: 25px 35px 10px 25px;
+
   @media (max-width: ${device.mobile}) {
     width: 100%;
     display: flex;
