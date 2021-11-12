5544c10978a22ae059de1df2bda2c39c99448456
bekzod9936 <bekzodnorkulov9936@gmail.com>
2021-11-12 18:37:34 +0500

    add : finance mobile design

--- src/pages/CompanyPages/finances/components/MobileTable/index.tsx
+++ src/pages/CompanyPages/finances/components/MobileTable/index.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next';
 import { useAppSelector } from 'services/redux/hooks';
-import { Container, Data, FullName, Title, Amount } from './style';
-
+import { Container, Data, FullName, Title, Amount, Wrapper } from './style';
+import FullModal from '../FullModal';
 const MobileTable = () => {
   const { t } = useTranslation();
   const data = useAppSelector((state) => state.finance.suggestionFinance.data);
@@ -10,11 +10,16 @@ const MobileTable = () => {
     <Container>
       {data.map((v: any) => {
         return (
-          <Data>
-            <FullName>{`${v?.firstName}  ${v?.lastName}`}</FullName>
-            <Title>{t('amountofpurchase')}:</Title>
-            <Amount> {v?.amount}</Amount>
-          </Data>
+          <>
+            <Data>
+              <FullName>{`${v?.firstName}  ${v?.lastName}`}</FullName>
+              <Wrapper>
+                <Title>{t('amountofpurchase')}:</Title>
+                <Amount> {` ${v?.amount}`}</Amount>
+              </Wrapper>
+            </Data>
+            <FullModal>ssss</FullModal>
+          </>
         );
       })}
     </Container>
--- src/pages/CompanyPages/finances/components/MobileTable/style.ts
+++ src/pages/CompanyPages/finances/components/MobileTable/style.ts
@@ -1,8 +1,21 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #ffffff;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.04);
+  border-radius: 12px;
+  padding: 5px 0;
+  & > div:nth-child(2n-1) {
+    background-color: #eff0fd;
+  }
+  & > div:nth-child(2n) {
+    background-color: white;
+  }
+`;
 
-export const Data = styled.div``;
+export const Data = styled.div`
+  padding: 10px 20px;
+`;
 
 export const FullName = styled.div`
   font-weight: normal;
@@ -20,4 +33,8 @@ export const Amount = styled.div`
   font-weight: 400;
   font-size: 14px;
   color: #223367;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
 `;
