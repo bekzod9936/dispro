701d135278f40eb684efa4c15ffcb523459d4c2d
dontmeway <bbakhadyrov@gmail.com>
2021-11-16 14:42:37 +0500

    fix: qrcode side bar Clients page

--- src/pages/CompanyPages/clients/components/QrCodeBar/index.tsx
+++ src/pages/CompanyPages/clients/components/QrCodeBar/index.tsx
@@ -1,17 +1,18 @@
 import { CloseIcon } from 'assets/icons/ClientsPageIcons/ClientIcons'
 import { useAppDispatch } from 'services/redux/hooks'
 import { setOpenSideBar } from 'services/redux/Slices/clients'
-import { CancelButton, Container, Content, LinkBtn, QrBlock, QrCode } from './style'
+import { CancelButton, Container, Content, LinkBtn, QrBlock } from './style'
 import { useState, useEffect } from "react"
 import { useMutation } from 'react-query'
 import { fetchQrCode } from 'services/queries/clientsQuery'
-
+import QrCode from "qrcode.react"
 
 export const QrCodeBar = () => {
     const dispatch = useAppDispatch()
     const [copied, setCopied] = useState(false)
     const [code, setCode] = useState<any>(null)
     const [link, setLink] = useState("")
+
     const handleCopy = () => {
         const el = document.createElement("input");
         el.value = link;
@@ -22,11 +23,22 @@ export const QrCodeBar = () => {
         setCopied(true)
     }
 
+    const { mutate } = useMutation(() => fetchQrCode(), {
+        retry: 0,
+        onSuccess: (data) => {
+            const res = data.data.data;
+            setLink(res.dynLinkToken);
+            setCode(res.token)
+
+        }
+    });
+
+    console.log(link);
+
     useEffect(() => {
-        const res: any = fetchQrCode()
-        setLink(res.dynLinkToken)
-        setCode(res)
+        mutate()
     }, [])
+
     return (
         <Container>
             <CancelButton onClick={(e) => dispatch(setOpenSideBar(false))}>
@@ -41,12 +53,10 @@ export const QrCodeBar = () => {
                     попадет в вашу базу.
                 </p>
                 <QrBlock>
-                    <QrCode>
-
-                    </QrCode>
-                    <p>qrcode id</p>
+                    {link && <QrCode size={150} value={link} />}
+                    <p>{code}</p>
                     <span>Ссылка на присоединение</span>
-                    <LinkBtn onClick={handleCopy}>https/:discount_the_best</LinkBtn>
+                    <LinkBtn onClick={handleCopy}>{link.length > 25 ? link.slice(0, 26) + "..." : link}</LinkBtn>
                     <button onClick={handleCopy}>{copied ? "Скопировано" : "Скопировать ссылку"}</button>
                 </QrBlock>
             </Content>
