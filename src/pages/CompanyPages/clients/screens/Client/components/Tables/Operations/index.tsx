34b35e84d756fdf35fb1d0ab862a75872d541a20
dontmeway <bbakhadyrov@gmail.com>
2021-11-26 10:33:26 +0500

    fix: clients page bugs

--- src/pages/CompanyPages/clients/screens/Client/components/Tables/Operations/index.tsx
+++ src/pages/CompanyPages/clients/screens/Client/components/Tables/Operations/index.tsx
@@ -105,19 +105,19 @@ const Operations = () => {
                     </Tbody>
                 </Table> :
                 <MobileTable>
-                    {Object.keys(mobileOperations).map((date: any, index: any) => (
-                        <MTRow isEven={!!((index + 1) % 2)}>
-                            <div className="date">{date}</div>
-                            {mobileOperations[date].map((el: any) => (
-                                <div className="content">
-                                    <div className="left">
-                                        <p className="type">{el.action}</p>
-                                        <p className="value">{el.value}</p>
-                                    </div>
-                                    <span className="time">{el.time}</span>
-                                </div>
-                            ))}
-                        </MTRow>
+                    {Object.keys(mobileOperations).map((date: any) => (
+                            <>{mobileOperations[date].map((el: any, index: number) => (
+                                <MTRow>
+                                    {index === 0 && <div className="date">{date}</div>}
+                                        <div className="content">
+                                            <div className="left">
+                                                <p className="type">{el.action}</p>
+                                                <p className="value">{el.value}</p>
+                                            </div>
+                                            <span className="time">{el.time}</span>
+                                        </div>
+                                </MTRow>
+                            ))}</>
                     ))}
                 </MobileTable>}
             <Footer>
