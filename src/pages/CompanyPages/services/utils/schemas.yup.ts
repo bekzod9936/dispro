b52544b78aad9d081947f8885ec6252d8821f211
dontmeway <bbakhadyrov@gmail.com>
2021-12-02 08:28:25 +0500

    fix: schema validation to check is field unique Services page

--- src/pages/CompanyPages/services/utils/schemas.yup.ts
+++ src/pages/CompanyPages/services/utils/schemas.yup.ts
@@ -1,10 +1,33 @@
 import * as yup from "yup"
 
 
+
 export const sectionsSchema = yup.object().shape({
     sections: yup.array().of(
         yup.object().shape({
-            name: yup.string().max(30, "maxAmountOfCharacters").required('requiredField')
+            title: yup.string().max(30, "maxAmountOfCharacters").required('requiredField')
         })
-    )
+    ).test('unique', 'titlesOfSectionsMustBeUnique', function (array) {
+        let res = array?.map(el => el.title);
+        const { createError, path } = this;
+        if (res?.length === new Set(res).size) return true;
+        else {
+            let index = 0;
+            let uniqueArray: string[] = [];
+            array?.forEach((el: any, indx: number) => {
+                if (!uniqueArray.includes(el.title)) {
+                    uniqueArray.push(el.title)
+                } else index = indx
+            })
+
+            throw createError({
+                message: "titlesOfSectionsMustBeUnique",
+                path: `${path}.${index}.title`
+            })
+        }
+
+    })
 })
+
+
+
