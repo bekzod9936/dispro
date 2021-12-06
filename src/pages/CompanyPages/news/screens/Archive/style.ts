1eee0c226feb70ba2cf23a3291f2bddcdd00038d
Javlonbek <suyunovjavlonbek@gmail.com>
2021-12-06 16:38:10 +0500

    fix news

--- src/pages/CompanyPages/news/screens/Archive/style.ts
+++ src/pages/CompanyPages/news/screens/Archive/style.ts
@@ -45,7 +45,7 @@ export const Wrap = styled.div`
     padding: 0px 15px;
   }
   @media (min-width: ${device.mobile}) and (max-width: ${device.planshet}) {
-    padding-top: 20px;
+    padding-top: 0px;
     padding-right: 25px;
     padding-left: 25px;
   }
