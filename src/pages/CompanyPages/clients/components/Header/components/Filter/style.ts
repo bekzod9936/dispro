549bc14c34efd58ba568320fb21cb516fe5b8d2f
dontmeway <bbakhadyrov@gmail.com>
2021-12-07 17:22:31 +0500

    fix: clients page tablet version

--- src/pages/CompanyPages/clients/components/Header/components/Filter/style.ts
+++ src/pages/CompanyPages/clients/components/Header/components/Filter/style.ts
@@ -4,7 +4,7 @@ import { device } from "styles/device";
 export const Wrapper = styled.div`
   margin-right: 20px;
   @media (max-width: ${device.planshet}) {
-    margin: 0 0 0 15px;
+    margin: 0 15px 0 0;
   }
   @media (max-width: ${device.mobile}) {
     margin: 0
