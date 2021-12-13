96306888cf1b92a1543ed20ece04d03be0a8f266
Umrzoq Toshkentov <utoshkentov@gmail.com>
2021-12-13 10:19:11 +0500

    fix: not founded page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -3,15 +3,12 @@ import React, { Fragment, Suspense } from "react";
 //app_routes
 import AuthRoutes from "./AuthRoutes/index";
 import PrivateRoutes from "./PrivateRoutes/index";
-import { Route } from "react-router-dom";
-import NotFound from "pages/NotFound";
 
 export const RenderAllRoutes: React.FC = () => {
   return (
     <Fragment>
       {AuthRoutes()}
       <Suspense fallback={<FallbackOnLazyLoad />}>{PrivateRoutes()}</Suspense>
-      <Route path="*" component={NotFound} />
     </Fragment>
   );
 };
