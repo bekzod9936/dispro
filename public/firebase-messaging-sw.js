aeeb3ef4d6251ea2c17d47a4b8b0e7794747dcb0
bekzod9936 <bekzodnorkulov9936@gmail.com>
2021-12-17 19:32:37 +0500

    fix: firebase 400 error

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,23 +1,38 @@
-importScripts("https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.6.5/firebase-messaging.js");
-console.warn("HERE IT IS !!");
+importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-messaging.js');
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAWYuvkVPG-htfbE1WJf1lWj05XUo2mpm4",
-  authDomain: "dis-count-f5f2a.firebaseapp.com",
-  projectId: "dis-count-f5f2a",
-  storageBucket: "dis-count-f5f2a.appspot.com",
-  messagingSenderId: "1013888749180",
-  appId: "1:1013888749180:web:d1eeac52de7d330b1343f9",
-  measurementId: "G-VET2ZEC1YD",
+  apiKey: 'AIzaSyAWYuvkVPG-htfbE1WJf1lWj05XUo2mpm4',
+  authDomain: 'dis-count-f5f2a.firebaseapp.com',
+  projectId: 'dis-count-f5f2a',
+  storageBucket: 'dis-count-f5f2a.appspot.com',
+  messagingSenderId: '1013888749180',
+  appId: '1:1013888749180:web:d1eeac52de7d330b1343f9',
+  measurementId: 'G-VET2ZEC1YD',
 };
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function (payload) {
-  console.log("Received background message ", payload);
+messaging.setBackgroundMessageHandler(function (payload) {
+  const promiseChain = clients
+    .matchAll({
+      type: 'window',
+      includeUncontrolled: true,
+    })
+    .then((windowClients) => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const windowClient = windowClients[i];
+        windowClient.postMessage(payload);
+      }
+    })
+    .then(() => {
+      return registration.showNotification('my notification title');
+    });
+  return promiseChain;
+});
 
+messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
--- src/pages/CompanyPages/statistics/useFcm.ts
+++ src/pages/CompanyPages/statistics/useFcm.ts
@@ -1,14 +1,13 @@
-import { useEffect } from "react";
-import { useMutation } from "react-query";
-import { setFcm } from "services/queries/authQuery";
-import { deviceDetect, getUA } from "react-device-detect";
-import { useAppSelector } from "services/redux/hooks";
+import { useEffect } from 'react';
+import { useMutation } from 'react-query';
+import { setFcm } from 'services/queries/authQuery';
+import { deviceDetect, getUA } from 'react-device-detect';
+import { useAppSelector } from 'services/redux/hooks';
 
 const useFcm = () => {
-  const fToken: any = localStorage.getItem("fcmToken");
+  const fToken: any = localStorage.getItem('fcmToken');
   const fcmToken = useAppSelector((state) => state.firebaseSlice.fcmToken);
   const deviceId = deviceDetect(getUA)?.userAgent;
-
   const fcmSetting = useMutation((data: any) => setFcm(data), {
     onSuccess: (data) => {
       // console.log(data.data, "data set fcm");
@@ -16,13 +15,13 @@ const useFcm = () => {
   });
 
   useEffect(() => {
-    if (!fcmToken) {
+    if (fToken !== null) {
       fcmSetting.mutate({
         fcmToken: fcmToken || fToken,
         deviceId: deviceId,
       });
     }
-  }, [fcmToken]);
+  }, [fToken]);
 
   return {
     fcmSetting,
