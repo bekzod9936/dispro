fb9db738af4ca17ea80381171e2c3ca109a25d1e
Shukhrat <41087745+shukhratgadoev@users.noreply.github.com>
2021-12-24 09:41:06 +0500

    fix: staff, cashiers filter

--- src/pages/CompanyPages/services/screens/Main/components/Header/index.tsx
+++ src/pages/CompanyPages/services/screens/Main/components/Header/index.tsx
@@ -1,73 +1,73 @@
 //react
-import React, { useState } from "react";
+import { useState } from 'react';
 
 //packages
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
 //components
-import Popover from "../../components/Popover";
-import Input from "components/Custom/Input";
-import { Flex } from "pages/CompanyPages/services/style";
-import Title from "components/Custom/Title";
-import { SectionModal } from "pages/CompanyPages/services/components/Modals/Sections";
+import Popover from '../../components/Popover';
+import Input from 'components/Custom/Input';
+import { Flex } from 'pages/CompanyPages/services/style';
+import Title from 'components/Custom/Title';
+import { SectionModal } from 'pages/CompanyPages/services/components/Modals/Sections';
 
 //style
-import { Nav, SearchIcon, useStyles, Wrapper } from "./style";
+import { Nav, SearchIcon, useStyles, Wrapper } from './style';
 
 //other
-import { useGetSections } from "pages/CompanyPages/services/hooks";
-import { getLengthOfParentSections } from "pages/CompanyPages/services/helpers";
-import { SECTIONS_LIMIT } from "pages/CompanyPages/services/constants";
+import { useGetSections } from 'pages/CompanyPages/services/hooks';
+import { getLengthOfParentSections } from 'pages/CompanyPages/services/helpers';
+import { SECTIONS_LIMIT } from 'pages/CompanyPages/services/constants';
 
 interface HeaderProps {
-  value: string;
-  onChange: (arg: string) => void;
+	value: string;
+	onChange: (arg: string) => void;
 }
 
 export const Header: React.FC<HeaderProps> = ({ value, onChange }) => {
-  const { t } = useTranslation();
-  const [createSection, setCreateSection] = useState<boolean>(false);
-  const styles = useStyles();
+	const { t } = useTranslation();
+	const [createSection, setCreateSection] = useState<boolean>(false);
+	const styles = useStyles();
 
-  const { data } = useGetSections();
-  const isSectionButtonDisabled =
-    SECTIONS_LIMIT - getLengthOfParentSections(data?.data) === 0;
+	const { data } = useGetSections();
+	const isSectionButtonDisabled =
+		SECTIONS_LIMIT - getLengthOfParentSections(data?.data) === 0;
 
-  const handleToggle = (bool: boolean) => {
-    return () => {
-      setCreateSection(bool);
-    };
-  };
+	const handleToggle = (bool: boolean) => {
+		return () => {
+			setCreateSection(bool);
+		};
+	};
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value);
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		onChange(event.target.value);
+	};
 
-  return (
-    <Wrapper>
-      <Nav>
-        <Title>{t("services")}</Title>
-        <p>
-          <b>•</b>
-          {t("youDontHaveProducts")}
-        </p>
-      </Nav>
-      <Flex>
-        <Popover
-          isSectionButtonDisabled={isSectionButtonDisabled}
-          onClick={handleToggle(true)}
-        />
-        <Input
-          value={value}
-          onChange={handleChange}
-          margin={styles.input.margin}
-          inputStyle={styles.input.style}
-          IconStart={<SearchIcon />}
-          width={styles.input.width}
-          placeholder={t("search")}
-        />
-      </Flex>
-      <SectionModal isOpen={createSection} onClose={handleToggle(false)} />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<Nav>
+				<Title>{t('services')}</Title>
+				<p>
+					<b>•</b>
+					{t('youDontHaveProducts')}
+				</p>
+			</Nav>
+			<Flex>
+				<Popover
+					isSectionButtonDisabled={isSectionButtonDisabled}
+					onClick={handleToggle(true)}
+				/>
+				<Input
+					value={value}
+					onChange={handleChange}
+					margin={styles.input.margin}
+					inputStyle={styles.input.style}
+					IconStart={<SearchIcon />}
+					width={styles.input.width}
+					placeholder={t('search')}
+				/>
+			</Flex>
+			<SectionModal isOpen={createSection} onClose={handleToggle(false)} />
+		</Wrapper>
+	);
 };
