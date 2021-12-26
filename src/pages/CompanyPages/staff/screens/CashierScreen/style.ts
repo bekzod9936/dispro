05f66d4478212d202ccf396a2f4ec1b61e996b12
Shukhrat <41087745+shukhratgadoev@users.noreply.github.com>
2021-12-26 10:13:13 +0500

    add: staff, manager cashier image not found

--- src/pages/CompanyPages/staff/screens/CashierScreen/style.ts
+++ src/pages/CompanyPages/staff/screens/CashierScreen/style.ts
@@ -191,3 +191,35 @@ export const NText = styled.p`
   overflow: hidden;
   text-overflow: ellipsis;
 `;
+
+export const WrapDef = styled.div`
+  display: flex;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  margin-top: 20px;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ImgDef = styled.img`
+  @media (max-width: ${device.mobile}) {
+    width: 40%;
+    height: 60%;
+    min-height: 150px;
+    min-width: 160px;
+  }
+`;
+
+export const DefDiv = styled.div`
+  font-weight: normal;
+  font-size: 18px;
+  text-align: center;
+  color: #223367;
+  max-width: 40%;
+  margin-top: 10px;
+  @media (max-width: ${device.mobile}) {
+    font-size: 14px;
+  }
+`;
