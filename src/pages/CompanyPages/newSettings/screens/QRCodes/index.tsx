45e0e114c79fc248076248c0b2219dab7be5a660
bekzzod9936 <bekzodnorkulov9936@gmail.com>
2021-12-28 16:01:44 +0500

    add: delete qrcode

--- src/pages/CompanyPages/newSettings/screens/QRCodes/index.tsx
+++ src/pages/CompanyPages/newSettings/screens/QRCodes/index.tsx
@@ -1,20 +1,24 @@
-import Input from 'components/Custom/Input';
-import Spinner from 'components/Custom/Spinner';
-import { useTranslation } from 'react-i18next';
-import { SearchIcon } from 'newassets/icons/icons';
-import { Container, Header, Body } from './style';
-import FilterQr from './components/FilterQr';
-import BoxQr from './components/BoxQr';
-import useQrcode from './useQrcode';
-import { useAppSelector } from 'services/redux/hooks';
-import CreateQrCode from './components/CreateQrCode';
+import Input from "components/Custom/Input";
+import Spinner from "components/Custom/Spinner";
+import { useTranslation } from "react-i18next";
+import { SearchIcon } from "newassets/icons/icons";
+import { Container, Header, Body } from "./style";
+import FilterQr from "./components/FilterQr";
+import BoxQr from "./components/BoxQr";
+import useQrcode from "./useQrcode";
+import { useAppSelector } from "services/redux/hooks";
+import CreateQrCode from "./components/CreateQrCode";
 
 const QrCodes = () => {
   const { t } = useTranslation();
 
-  const { response } = useQrcode();
+  const { resRefQrCodes, resBranchesQrCodes } = useQrcode();
 
   const data = useAppSelector((state) => state.newsetting.refQrcodes);
+
+  const dataBranchQrcodes = useAppSelector(
+    (state) => state.newsetting.branchQrcodes
+  );
 
   return (
     <Container>
@@ -24,26 +28,46 @@ const QrCodes = () => {
         <Input
           IconStart={<SearchIcon />}
           inputStyle={{
-            shadow: '0px 4px 4px rgba(0, 0, 0, 0.04)',
-            border: 'none',
-            outpadding: '0 0 0 20px',
+            shadow: "0px 4px 4px rgba(0, 0, 0, 0.04)",
+            border: "none",
+            outpadding: "0 0 0 20px",
             height: { desktop: 50, laptop: 45 },
           }}
-          type='search'
-          placeholder={t('searchbyqrcode')}
-          margin={{ laptop: '0 0 0 20px', mobile: '0 20px' }}
+          type="search"
+          placeholder={t("searchbyqrcode")}
+          margin={{ laptop: "0 0 0 20px", mobile: "0 20px" }}
           width={{ maxwidth: 500 }}
         />
       </Header>
       <Body>
-        {response.isLoading ? (
+        {resRefQrCodes.isLoading && resBranchesQrCodes.isLoading ? (
           <Spinner />
         ) : (
           <div>
             {data?.map((v: any) => (
-              <BoxQr key={v.id} value={v} />
+              <BoxQr
+                key={v.id}
+                link={v.dynLinkToken}
+                name={v.source}
+                id={v.id}
+              />
             ))}
-            <div style={{ height: '5px', width: '100%' }} />
+            {dataBranchQrcodes?.map((v: any) => {
+              if (v.active) {
+                return (
+                  <BoxQr
+                    key={v.id}
+                    link={v.dynLink}
+                    name={v.name}
+                    id={v.id}
+                    branch={true}
+                  />
+                );
+              } else {
+                return null;
+              }
+            })}
+            <div style={{ height: "5px", width: "100%" }} />
           </div>
         )}
       </Body>
--- src/services/queries/newSettingQueries/index.ts
+++ src/services/queries/newSettingQueries/index.ts
@@ -1,34 +1,57 @@
-import partnerApi from 'services/interceptors/partner_interceptor';
+import partnerApi from "services/interceptors/partner_interceptor";
 
 //rewarding
 
 export const fetchRewards = () => {
-  const response = partnerApi.get('/bonus/rewards');
+  const response = partnerApi.get("/bonus/rewards");
   return response;
 };
 
 export const postRewards = (data: any) => {
-  const response = partnerApi.post('/bonus/rewards', data);
+  const response = partnerApi.post("/bonus/rewards", data);
   return response;
 };
 
 //security
 
 export const fetchSecurity = () => {
-  const response = partnerApi.get('/core/company-safeties');
+  const response = partnerApi.get("/core/company-safeties");
   return response;
 };
 
 export const postSecurity = (data: any) => {
-  const response = partnerApi.put('/core/company-safeties', data);
-
+  const response = partnerApi.put("/core/company-safeties", data);
   return response;
 };
-
 
 //qrcodes
 
 export const fetchQRCodes = () => {
-  const response = partnerApi.get('/core/ref');
+  const response = partnerApi.get("/core/ref");
+  return response;
+};
+
+export const fetchBranchesQrCode = () => {
+  const response = partnerApi.get("/directory/stores/qr-codes");
+  return response;
+};
+
+export const putBranchesQuery = (data: any) => {
+  const response = partnerApi.put("/directory/company/generate/qr-code", data);
+  return response;
+};
+
+export const postRefQuery = async (data: any) => {
+  const response = await partnerApi.post("/core/ref", data);
+  return response;
+};
+
+export const refDeleteQrCode = async (data: any) => {
+  const response = await partnerApi.delete("/core/ref", data);
+  return response;
+};
+
+export const refEditQrCode = async (data: any) => {
+  const response = await partnerApi.put("/core/ref", data);
   return response;
 };
