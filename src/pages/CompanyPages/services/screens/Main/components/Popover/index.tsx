e3d32e063e5ec4f60d48f92fc79dc240966ffc29
dontmeway <bbakhadyrov@gmail.com>
2022-01-10 15:43:27 +0500

    fix: popover does not work when anchorEl disabled Services page

--- src/pages/CompanyPages/services/screens/Main/components/Popover/index.tsx
+++ src/pages/CompanyPages/services/screens/Main/components/Popover/index.tsx
@@ -4,11 +4,10 @@ import { useState } from "react";
 //packages
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-import { PopupState } from "material-ui-popup-state/core";
 
 //components
-import Popover from "components/Custom/Popover";
 import Button from "components/Custom/Buttons/Button";
+import { Popover } from "@material-ui/core";
 
 //style
 import {
@@ -29,7 +28,7 @@ const MPopover: React.FC<MPopoverProps> = ({
   onClick,
   isSectionButtonDisabled,
 }) => {
-  const [anchorEl, setAnchorEl] = useState<null | PopupState>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLButtonElement>(null);
   const styles = useStyles();
 
   const { t } = useTranslation();
@@ -39,50 +38,66 @@ const MPopover: React.FC<MPopoverProps> = ({
     history.push("/services/create");
   };
 
-  const handleClose = (e: PopupState) => {
-    setAnchorEl(e);
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
   const onOpenModal = () => {
     if (isSectionButtonDisabled) return;
 
     onClick();
-    anchorEl?.close();
+    handleClose();
   };
 
   return (
-    <Popover
-      click={
-        <Button
-          startIcon={<CreateIcon />}
-          buttonStyle={styles.popover.button.style}
-          endIcon={<ArrowDownIcon />}
-        >
-          {t("create")}
-        </Button>
-      }
-      onClose={handleClose}
-      anchorOrigin={{ horizontal: "left", vertical: "bottom" }}
-      transformOrigin={{ horizontal: "left", vertical: "top" }}
-      popoverStyle={styles.popover.menu.style}
-    >
-      <PopoverList>
-        <LightToolTip
-          arrow
-          placement="top"
-          title={
-            isSectionButtonDisabled
-              ? "Вы уже создали максимальное количество(20) разделов!"
-              : ""
-          }
-        >
-          <PopoverItem disabled={isSectionButtonDisabled} onClick={onOpenModal}>
-            {t("section")}
-          </PopoverItem>
-        </LightToolTip>
-        <PopoverItem onClick={handlePush}>{t("item")}</PopoverItem>
-      </PopoverList>
-    </Popover>
+    <>
+      <Button
+        onClick={handleOpen}
+        startIcon={<CreateIcon />}
+        buttonStyle={styles.popover.button.style}
+        endIcon={<ArrowDownIcon />}
+      >
+        {t("create")}
+      </Button>
+      <Popover
+        PaperProps={styles.popover}
+        open={Boolean(anchorEl)}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <PopoverList>
+          <LightToolTip
+            arrow
+            placement="top"
+            title={
+              isSectionButtonDisabled
+                ? "Вы уже создали максимальное количество(20) разделов!"
+                : ""
+            }
+          >
+            <PopoverItem
+              disabled={isSectionButtonDisabled}
+              onClick={onOpenModal}
+            >
+              {t("section")}
+            </PopoverItem>
+          </LightToolTip>
+          <PopoverItem onClick={handlePush}>{t("item")}</PopoverItem>
+        </PopoverList>
+      </Popover>
+    </>
   );
 };
 
