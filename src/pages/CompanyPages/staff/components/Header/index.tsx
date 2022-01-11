71bd0c2d75f0dd6c1a7568719d00aeec0d642b89
Shukhrat <41087745+shukhratgadoev@users.noreply.github.com>
2022-01-11 13:07:28 +0500

    fix: staff  permissions

--- src/pages/CompanyPages/staff/components/Header/index.tsx
+++ src/pages/CompanyPages/staff/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { Popover } from '@material-ui/core';
 
 //helpers
 import {
@@ -10,11 +11,11 @@ import {
 	setSelectedCashiers,
 } from 'services/redux/Slices/staffs';
 import { useAppSelector, useAppDispatch } from 'services/redux/hooks';
+import { usePermissions } from 'services/hooks/usePermissions';
 
 //components
 import Button from 'components/Custom/Buttons/Button';
 import Input from 'components/Custom/Input';
-import Popover from 'components/Custom/Popover';
 
 //styles
 import { SearchIcon } from 'components/Layout/Header/style';
@@ -28,11 +29,11 @@ import { ReactComponent as ArrowDown } from 'assets/icons/arrow_down.svg';
 import { ReactComponent as SettingsIcon } from 'assets/icons/settings_icon.svg';
 import { ReactComponent as FilterIcon } from 'assets/icons/StatistisPage/filter.svg';
 import { AddIcon } from 'assets/icons/InfoPageIcons/InfoPageIcons';
-import { useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const Header = ({
-	handleOpen,
-	handleClose,
+	// handleOpen,
+	// handleClose,
 	closeFun,
 	handleOpenSetting,
 }: IProps) => {
@@ -40,11 +41,30 @@ const Header = ({
 	const dispatch = useAppDispatch();
 	const query = useAppSelector((state) => state.staffs.query);
 	const open = useAppSelector((state) => state.staffs.openFilter);
+	const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
 	const { t } = useTranslation();
-
+	const isEditable = usePermissions('staff');
 	useEffect(() => {
 		dispatch(setQuery(''));
 	}, [location.pathname]);
+
+	const handleOpen = useCallback(
+		(event: React.MouseEvent<HTMLButtonElement>) => {
+			setAnchorEl(event.currentTarget);
+		},
+		[]
+	);
+
+	const handleClose = useCallback(() => {
+		setAnchorEl(null);
+	}, []);
+
+	const handleOpenModal = (modal: 'cashier' | 'manager') => {
+		if (modal === 'cashier') dispatch(setOpenCash(true));
+		else dispatch(setOpenManager(true));
+
+		handleClose();
+	};
 
 	return (
 		<Flex
@@ -53,57 +73,54 @@ const Header = ({
 			alignItems='center'
 			margin='0'
 		>
-			<Popover
-				click={
-					<Button
-						onClick={handleOpen}
-						buttonStyle={{
-							bgcolor: '#FFFFFF',
-							color: '#223367',
-							weight: 500,
-							height: { desktop: 60 },
-						}}
-						margin={{
-							desktop: '0 25px 0 0',
-							laptop: '0 25px 0 0',
-							planshet: '0 0 20px 0',
-						}}
-						startIcon={<AddIcon />}
-						endIcon={<ArrowDown />}
-					>
-						{t('create')}
-					</Button>
-				}
-				anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
-				transformOrigin={{ horizontal: 'left', vertical: 'top' }}
-				popoverStyle={{ marginTop: '20px' }}
-				onClose={handleClose}
-			>
-				<StaffPopover>
-					<PopoverRow
-						onClick={() => {
-							closeFun?.close();
-							dispatch(setOpenCash(true));
-						}}
-					>
-						{t('cashier')}
-					</PopoverRow>
+			<>
+				<Button
+					disabled={!isEditable}
+					onClick={handleOpen}
+					buttonStyle={{
+						bgcolor: '#FFFFFF',
+						color: '#223367',
+						weight: 500,
+						height: { desktop: 60 },
+					}}
+					margin={{
+						desktop: '0 25px 0 0',
+						laptop: '0 25px 0 0',
+						planshet: '0 0 20px 0',
+					}}
+					startIcon={<AddIcon />}
+					endIcon={<ArrowDown />}
+				>
+					{t('create')}
+				</Button>
+				<Popover
+					open={Boolean(anchorEl)}
+					onClose={handleClose}
+					anchorEl={anchorEl}
+					anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+					transformOrigin={{ horizontal: 'left', vertical: 'top' }}
+					PaperProps={{
+						style: {
+							marginTop: 10,
+						},
+					}}
+				>
+					<StaffPopover>
+						<PopoverRow onClick={() => handleOpenModal('cashier')}>
+							{t('cashier')}
+						</PopoverRow>
 
-					<PopoverRow
-						onClick={() => {
-							closeFun?.close();
-							dispatch(setOpenManager(true));
-						}}
-						light={true}
-					>
-						{t('manager')}
-					</PopoverRow>
-				</StaffPopover>
-			</Popover>
+						<PopoverRow onClick={() => handleOpenModal('manager')} light={true}>
+							{t('manager')}
+						</PopoverRow>
+					</StaffPopover>
+				</Popover>
+			</>
 
 			{/* Settings side  */}
 			{location.pathname !== '/staff/manager' && (
 				<Button
+					disabled={!isEditable}
 					onClick={
 						location.pathname !== '/staff/manager/setting'
 							? handleOpenSetting
--- src/pages/CompanyPages/staff/screens/CashierCard/screens/CashierBalls/index.tsx
+++ src/pages/CompanyPages/staff/screens/CashierCard/screens/CashierBalls/index.tsx
@@ -1,18 +1,18 @@
 import { IProps, FormProps } from './types';
 import { useForm, Controller } from 'react-hook-form';
 import {
-  Container,
-  Content,
-  ContentHead,
-  ContentBody,
-  HeadText,
-  Text,
-  ContentRow,
-  BallText,
-  SecondText,
-  Form,
-  ActDiv,
-  FormCol,
+	Container,
+	Content,
+	ContentHead,
+	ContentBody,
+	HeadText,
+	Text,
+	ContentRow,
+	BallText,
+	SecondText,
+	Form,
+	ActDiv,
+	FormCol,
 } from './style';
 import { numberWith } from 'services/utils';
 import InputFormat from 'components/Custom/InputFormat';
@@ -20,88 +20,90 @@ import Button from 'components/Custom/Buttons/Button';
 import { DownloadIcon } from 'assets/icons/SettingsIcons/SettingsPageIcon';
 import useCashierCard from '../../hooks/useCashierCard';
 import { useAppSelector } from 'services/redux/hooks';
+import { usePermissions } from 'services/hooks/usePermissions';
 
 const CashierBalls = ({ ballCount = 0 }: IProps) => {
-  const { resetPoint } = useCashierCard();
-  const cashierId = useAppSelector((state) => state.staffs.cashierId);
-  const { staffData, cashiers } = useAppSelector((state) => state.staffs);
-  console.log(`staffData`, staffData);
-  const {
-    handleSubmit,
-    control,
-    formState: { isValid, errors },
-  } = useForm<FormProps>({
-    mode: 'onChange',
-    shouldFocusError: true,
-    reValidateMode: 'onChange',
-  });
+	const { resetPoint } = useCashierCard();
+	const cashierId = useAppSelector((state) => state.staffs.cashierId);
+	const { staffData, cashiers } = useAppSelector((state) => state.staffs);
+	const isEditable = usePermissions('staff');
+	console.log(`staffData`, staffData);
+	const {
+		handleSubmit,
+		control,
+		formState: { isValid, errors },
+	} = useForm<FormProps>({
+		mode: 'onChange',
+		shouldFocusError: true,
+		reValidateMode: 'onChange',
+	});
 
-  const onSave = (data: FormProps) => {
-    resetPoint.mutate({
-      pointValue: +data.ballAmount,
-      cashierStaffId: +cashierId,
-      cashierUserId: staffData?.cashierUserId,
-    });
-  };
+	const onSave = (data: FormProps) => {
+		resetPoint.mutate({
+			pointValue: +data.ballAmount,
+			cashierStaffId: +cashierId,
+			cashierUserId: staffData?.cashierUserId,
+		});
+	};
 
-  return (
-    <Container>
-      <Content>
-        <ContentHead>
-          <HeadText>Остаток</HeadText>
-          <Text>Накоплено</Text>
-          <Text>Списано</Text>
-        </ContentHead>
-        <ContentBody>
-          <ContentRow>
-            <BallText>{numberWith(ballCount, ' ')}</BallText>
-            <SecondText>0</SecondText>
-            <SecondText>0</SecondText>
-          </ContentRow>
-          <ContentRow>
-            <Form onSubmit={handleSubmit(onSave)}>
-              <FormCol>
-                <Controller
-                  name='ballAmount'
-                  control={control}
-                  rules={{
-                    required: true,
-                  }}
-                  render={({ field }) => {
-                    return (
-                      <InputFormat
-                        label={'Колличество баллов'}
-                        type='string'
-                        field={field}
-                        max={parseInt(ballCount).toString()}
-                        fullWidth={true}
-                        width={{
-                          width: '100%',
-                        }}
-                        margin={{
-                          laptop: '30px 0 0',
-                        }}
-                        error={errors.ballAmount}
-                      />
-                    );
-                  }}
-                />
-              </FormCol>
-              <ActDiv>
-                <Button
-                  disabled={!isValid || resetPoint.isLoading}
-                  startIcon={<DownloadIcon />}
-                  type='submit'
-                >
-                  Списать баллы
-                </Button>
-              </ActDiv>
-            </Form>
-          </ContentRow>
-        </ContentBody>
-      </Content>
-    </Container>
-  );
+	return (
+		<Container>
+			<Content>
+				<ContentHead>
+					<HeadText>Остаток</HeadText>
+					<Text>Накоплено</Text>
+					<Text>Списано</Text>
+				</ContentHead>
+				<ContentBody>
+					<ContentRow>
+						<BallText>{numberWith(ballCount, ' ')}</BallText>
+						<SecondText>0</SecondText>
+						<SecondText>0</SecondText>
+					</ContentRow>
+					<ContentRow>
+						<Form onSubmit={handleSubmit(onSave)}>
+							<FormCol>
+								<Controller
+									name='ballAmount'
+									control={control}
+									rules={{
+										required: true,
+									}}
+									render={({ field }) => {
+										return (
+											<InputFormat
+												label={'Колличество баллов'}
+												type='string'
+												field={field}
+												max={parseInt(ballCount).toString()}
+												fullWidth={true}
+												width={{
+													width: '100%',
+												}}
+												margin={{
+													laptop: '30px 0 0',
+												}}
+												error={errors.ballAmount}
+											/>
+										);
+									}}
+								/>
+							</FormCol>
+							<ActDiv>
+								<Button
+									disabled={!isValid || resetPoint.isLoading || !isEditable}
+									startIcon={<DownloadIcon />}
+									type='submit'
+								>
+									Списать баллы
+								</Button>
+							</ActDiv>
+						</Form>
+					</ContentRow>
+				</ContentBody>
+			</Content>
+		</Container>
+	);
 };
 
 export default CashierBalls;
